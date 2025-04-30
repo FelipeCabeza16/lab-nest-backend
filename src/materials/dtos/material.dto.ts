f1e7874cf35819feb87c9205c18c325fa228850a
Andrés Cabeza <andres.cabeza@idrd.gov.co>
2025-04-30 12:11:34 -0500

    feat: update material dto

--- src/materials/dtos/material.dto.ts
+++ src/materials/dtos/material.dto.ts
@@ -40,14 +40,7 @@ export class CreateMaterialDto {
   price: number;
 }
 
-export class UpdateMaterialDto extends CreateMaterialDto {
-  @ApiProperty({
-    description: 'UUID of the material',
-    example: '123e4567-e89b-12d3-a456-426614174000',
-  })
-  @IsUUID()
-  id: string;
-}
+export class UpdateMaterialDto extends CreateMaterialDto {}
 
 export class MaterialResponseDto extends UpdateMaterialDto {
   @ApiProperty({
